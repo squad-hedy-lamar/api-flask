7d655e43a3cb4f93dd059b2883fa6e309bc64d11
Daniela2319 <frankvt04@gmail.com>
2024-09-06 17:48:23 -0300

    Adicionando html

--- routes/episodes.py
+++ routes/episodes.py
@@ -1,46 +1,48 @@
 from main import app
-from flask import render_template,  request, jsonify
+from flask import render_template, request, jsonify
 from utils.config import api_url
 import urllib.request
 import json
 
 API_URL = api_url()
 
+
 @app.route("/episodes")
 def get_episodes():
-  # fazer aqui a chama da API
-  url = f"{API_URL}/episode"
-  response = urllib.request.urlopen(url)
-  episodes = response.read().decode("utf-8")
-  data = json.loads(episodes)
-    
-  episodes_list = []
-  
-  for episode in data['results']:
-    episode_info = {
-      "id": episode['id'],
-      "name": episode['name'],
-      "air_date": episode['air_date'],
-      "episode": episode['episode']
-  }
-    episodes_list.append(episode_info)
-  
-  return render_template('/episodes/list.html', active_tab='episodes', data={"episodes": episodes_list})
- 
-  
+    # fazer aqui a chama da API
+    url = f"{API_URL}/episode"
+    response = urllib.request.urlopen(url)
+    episodes = response.read().decode("utf-8")
+    data = json.loads(episodes)
+
+    episodes_list = []
+
+    for episode in data["results"]:
+        episode_info = {
+            "id": episode["id"],
+            "name": episode["name"],
+            "air_date": episode["air_date"],
+            "episode": episode["episode"],
+        }
+        episodes_list.append(episode_info)
+
+    return render_template(
+        "/episodes/list.html", active_tab="episodes", data={"episodes": episodes_list}
+    )
+
+
 @app.route("/episodes/<id>")
 def get_episode(id):
-  # fazer aqui a chama da API
-  url = f"{API_URL}/episode/{id}"
-  response = urllib.request.urlopen(url)
-  episode = response.read().decode("utf-8")
-  data = json.loads(episode)
-  
-  # Prepare a lista de personagens
-  #characters_list = [char_url for char_url in data['characters']]  
-    
-  return render_template('/episodes/details.html', active_tab='episodes', data={"episode": data})
+    # fazer aqui a chama da API
+    url = f"{API_URL}/episode/{id}"
+    response = urllib.request.urlopen(url)
+    episode = response.read().decode("utf-8")
+    data = json.loads(episode)
+
+    return render_template(
+        "/episodes/details.html", active_tab="episodes", data={"episode": data}
+    )
 
 
-if __name__ == '__main__':
-  app.run(debug=True)
+if __name__ == "__main__":
+    app.run(debug=True)
