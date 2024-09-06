e68861ec9621cffd6119b65750da49adc6b4756b
jadefigueredo <jade.figueredo@jusbrasil.com.br>
2024-09-06 16:06:08 -0300

    add characters.py

--- routes/characters.py
+++ routes/characters.py
@@ -9,10 +9,38 @@ API_URL = api_url()
 @app.route("/characters")
 def get_characters():
   # fazer aqui a chamada da API
-  return render_template('/characters/list.html', active_tab='characters', data={})
+  url = f"{API_URL}/characters"
+  response = urllib.request.urlopen(url)
+  characters = response.read().decode("utf-8")
+  data = json.loads(characters)
 
+  characters_list = []
+
+  for character in data['results']:
+    character_info = {
+      "id": character['id'],
+      "name": character['name'],
+      "species": character['species'],
+      "gender": character['gender'],
+      "origin": character['origin'],
+      "location": character['location']
+    }
+    characters_list.append(character_info)
+
+  return render_template('/characters/list.html', active_tab='characters', data={"chacters": characters_list})
 
 @app.route("/characters/<id>")
 def get_character(id):
   # fazer aqui a chamada da API
-  return render_template('/characters/details.html', active_tab='characters', data={})
+  url = f"{API_URL}/characters/{id}"
+  response = urllib.request.urlopen(url)
+  character = response.read().decode("utf-8")
+  data = json.loads(character)
+
+  # lista de episódios
+  episodes_list = [episode_url for episode_url in data['episode']]
+
+  return render_template('/characters/details.html', active_tab='characters', data={"character": data, "episodes": episodes_list})
+
+if __name__ == '__main__':
+  app.run(debug=True)
