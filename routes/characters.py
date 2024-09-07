f65fe9f0e759a3bf3510c92ab47dd926146a7e89
Emanoela Santos <OliveiraEmanoelaS@JohnDeere.com>
2024-09-07 13:28:38 -0300

    edit some code in the charascters.py

--- routes/characters.py
+++ routes/characters.py
@@ -10,6 +10,7 @@ API_URL = api_url()
 
 @app.route("/character")
 def get_characters():
+    print("get_characters function called")  # Adicione esta linha para verificar se a função é chamada
     # fazer aqui a chamada da API
     url = f"{API_URL}/character"
     response = urllib.request.urlopen(url)
@@ -24,31 +25,18 @@ def get_characters():
             "name": character["name"],
             "species": character["species"],
             "gender": character["gender"],
-            "origin": character["origin"],
-            "location": character["location"],
+            "origin": character["origin"]["name"],
+            "location": character["location"]["name"],
+            "image": character["image"]
         }
         characters_list.append(character_info)
 
         return render_template(
-            "/characters/list.html",
+            "characters/list.html",
             active_tab="characters",
             data={"chacters": characters_list},
         )
-
-    characters_list = []
-
-    for character in data['results']:
-      character_info = {
-        "id": character['id'],
-        "name": character['name'],
-        "species": character['species'],
-        "gender": character['gender'],
-        "origin": character['origin'],
-        "location": character['location']
-      }
-      characters_list.append(character_info)
-    return render_template('/characters/list.html', active_tab='characters', data={"chacters": characters_list})
-
+    print("Characters List:", characters_list)
 @app.route("/character/<id>")
 def get_character(id):
     # fazer aqui a chamada da API
